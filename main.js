a4221d0eae38dfd4628e10132f36098282b5e5f6
akhisulaymaan <54278218+akhisulaymaan@users.noreply.github.com>
2019-12-21 23:01:42 +0100

    chap 3

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-/*const btn = document.getElementById('button');
-const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'rebeccablue', 'violet'];
-
-function change() {
-    document.body.style.background = rainbow[Math.floor(7*Math.random())];
-}
-    btn.addEventListener('click', change); */
-
-
 alert('Welcome to Quiz Ninja!');
 
-const question = "What is Superman's real name?" 
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+const quiz = [ ["What is Superman's real name?","Clark Kent"], 
+["What is Wonder Woman's real name?","Diana Prince"], 
+["What is Batman's real name?","Bruce Wayne"] ];
+
+let score = 0;
+
+for(const [question,answer] of quiz){ 
+    const response = prompt(question); 
+    if(response === answer){ 
+        alert('Correct!'); score++; 
+    } else { 
+        alert(`Wrong! The correct answer was ${answer}`); 
+    } 
+}
+
+alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
