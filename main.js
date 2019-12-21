08eab9151a3706e38f6e78af019c5d742f5930ad
akhisulaymaan <54278218+akhisulaymaan@users.noreply.github.com>
2019-12-21 22:56:28 +0100

    chap 1 and 2

---
+++ main.js
@@ -0,0 +1,14 @@
+/*const btn = document.getElementById('button');
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'rebeccablue', 'violet'];
+
+function change() {
+    document.body.style.background = rainbow[Math.floor(7*Math.random())];
+}
+    btn.addEventListener('click', change); */
+
+
+alert('Welcome to Quiz Ninja!');
+
+const question = "What is Superman's real name?" 
+const answer = prompt(question);
+alert(`You answered ${answer}`);
